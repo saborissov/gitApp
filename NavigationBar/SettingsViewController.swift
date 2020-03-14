1cc2c0ff2ab317bfefaed51422421663e6d84ed8
Sergey Borisov <it.saborisov@gmail.com>
2020-03-14 23:15:22 +0300

    addNewViewController

---
+++ NavigationBar/SettingsViewController.swift
@@ -0,0 +1,22 @@
+//
+//  SettingsViewController.swift
+//  NavigationBar
+//
+//  Created by Сергей Борисов on 14.03.2020.
+//  Copyright © 2020 Сергей Борисов. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    @IBAction func goToNext(_ sender: UIButton) {
+        let newVC = storyboard?.instantiateViewController(withIdentifier: "NewViewController")
+        navigationController?.pushViewController(newVC!, animated: true)
+    }
+}
