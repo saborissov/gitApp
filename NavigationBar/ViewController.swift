f545eca1687dd6546b03d7d82839d88dcb12896f
Sergey Borisov <it.saborisov@gmail.com>
2020-03-14 07:21:48 +0300

    Initial Commit

---
+++ NavigationBar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NavigationBar
+//
+//  Created by Сергей Борисов on 14.03.2020.
+//  Copyright © 2020 Сергей Борисов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
